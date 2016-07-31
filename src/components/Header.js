6c9842e8d092f36f831477361a1f3372ad6556cc
Dean Silfen <dean.silfen@gmail.com>
2016-07-31 15:46:43 -0400

    Add some flare to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,19 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => {
+  const titles = [
+    'Regular Music for Regular People',
+    'Game Recognize Game',
+    'Real Recognize Real',
+    'No Pare, Sigue! Sigue!',
+    'Don\'t stop, Get it! Get it!',
+    'New Music, Regularly!'
+  ]
+  const element = Math.floor(Math.random() * titles.length)
   return (
     <div className="Header">
       <h2>Regularly</h2>
-      <h4>Regular Music for Regular People</h4>
+      <h4>{titles[element]}</h4>
     </div>
   )
 }
