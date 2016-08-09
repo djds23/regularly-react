1631bdee9613031edf23e5602907e421c9035217
Dean Silfen <dean.silfen@gmail.com>
2016-08-08 23:56:26 -0400

    Add facebook init code to Layout#componentDidMount
    add some login code

--- src/Layout.js
+++ src/Layout.js
@@ -4,6 +4,29 @@ import Header from './components/Header.js'
 import './App.css'
 
 class Layout extends React.Component {
+  componentDidMount () {
+    window.fbAsyncInit = () => {
+      FB.init({
+        appId      : '1081772241909189',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.7'
+      })
+
+      FB.getLoginStatus((response) => {
+        this.statusChangeCallback(response);
+      })
+    }
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+
   render () {
     return (
       <div className='App'>
--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,18 @@
 import React from 'react'
+import './Login.css'
 
 class Login extends React.Component {
   render () {
     return (
-      <div>
-        <form method='post'>
-          <label> Username
-            <input type='text' />
-          </label>
-          <label> Password
-            <input type='password' />
-          </label>
-          <button type='submit' value='Submit' />
-        </form>
+      <div className="loginContainer" >
+        <p>Login with your Facebook account</p>
+        <div
+          className="fb-login-button"
+          data-max-rows="1"
+          data-size="large"
+          data-show-faces="false"
+          data-auto-logout-link="false" >
+        </div>
       </div>
     )
   }
